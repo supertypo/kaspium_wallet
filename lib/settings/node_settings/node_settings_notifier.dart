ad7000eeb4c9ec2bcff32b0f11970718ccebc52e
Azbuky <azbuky@gmail.com>
2026-08-11 14:26:25 +0300

    Remove temporary Toccata node override

--- lib/settings/node_settings/node_settings_notifier.dart
+++ lib/settings/node_settings/node_settings_notifier.dart
@@ -6,52 +6,16 @@ import 'node_types.dart';
 
 const kNodeConfigKey = '_kNodeConfigKeyV2';
 
-// temporary Toccata node override
-final toccataNodeConfig = mainnetNodeConfig.copyWith(
-  urls: ['toccata.kaspium.io'],
-);
-
-NodeConfig _toccataOverride(NodeConfig config) {
-  if (config.id == mainnetNodeConfig.id) {
-    return toccataNodeConfig;
-  }
-  return config;
-}
-
-NodeConfig _toccataRevertOverride(NodeConfig config) {
-  if (config.id == toccataNodeConfig.id) {
-    return mainnetNodeConfig;
-  }
-  return config;
-}
-
-NodeConfigSettings _settingsOverride(NodeConfigSettings settings) {
-  return NodeConfigSettings(
-    options: settings.options.map(_toccataOverride).toIList(),
-    selected: _toccataOverride(settings.selected),
-  );
-}
-
-NodeConfigSettings _settingsRevertOverride(NodeConfigSettings settings) {
-  return NodeConfigSettings(
-    options: settings.options.map(_toccataRevertOverride).toIList(),
-    selected: _toccataRevertOverride(settings.selected),
-  );
-}
-
 extension NodeSettingsExtension on SettingsRepository {
   NodeConfigSettings getNodeConfigSettings() {
-    final settings =
-        box.tryGet<NodeConfigSettings>(
+    return box.tryGet<NodeConfigSettings>(
           kNodeConfigKey,
           typeFactory: NodeConfigSettings.fromJson,
         ) ??
         const NodeConfigSettings();
-    return _settingsOverride(settings);
   }
 
   Future<void> setNodeConfigSettings(NodeConfigSettings settings) {
-    settings = _settingsRevertOverride(settings);
     return box.set(kNodeConfigKey, settings);
   }
 }
